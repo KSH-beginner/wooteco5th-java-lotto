6a8de211122b8ad79d996ddbbded721266481fd6
ksh-beginner <ohk9134@naver.com>
2022-12-05 17:35:29 +0900

    refactor(OutputView) : 로또 구입 금액 입력 문구 출력 기능 메소드명 리팩토링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,7 +4,7 @@ public class OutputView {
 
     private static final String INPUT_LOTTO_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
 
-    public void inputLottoPurchaseMoneyMessage() {
+    public void printInputLottoPurchaseMoneyMessage() {
         printMessage(INPUT_LOTTO_PURCHASE_MONEY_MESSAGE);
     }
 
