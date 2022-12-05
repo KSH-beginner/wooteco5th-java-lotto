f3fdf6b011e6b3b0890672c5d55067f6c8ab3989
ksh-beginner <ohk9134@naver.com>
2022-12-05 17:32:58 +0900

    feat(OutputView) : 로또 구입 금액 입력 문구 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String INPUT_LOTTO_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void inputLottoPurchaseMoneyMessage() {
+        printMessage(INPUT_LOTTO_PURCHASE_MONEY_MESSAGE);
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
