cb8d4b64394754464d06ea79c170b1c9bd4a327c
ksh-beginner <ohk9134@naver.com>
2022-12-05 17:24:43 +0900

    feat(InputView) : 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,9 +2,21 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    private static final String LOTTO_WINNING_NUMBERS_SPLIT_DELIMITER = ",";
 
     public String inputLottoPurchaseMoney() {
         return Console.readLine();
+    }
+
+    public List<String> inputLottoWinningNumbers() {
+        String lottoWinningNumbers = Console.readLine();
+        return Arrays.stream(lottoWinningNumbers.split(LOTTO_WINNING_NUMBERS_SPLIT_DELIMITER))
+                .collect(Collectors.toList());
     }
 }
