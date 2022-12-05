7c2b3d536b856691d7c476c7ad64f5c8c6ea417c
ksh-beginner <ohk9134@naver.com>
2022-12-05 17:50:28 +0900

    refactor(LottoNumbersGenerator) : 로또 번호 발행 시 오름차순하여 반환하도록 리팩토링

--- src/main/java/lotto/LottoNumbersGenerator.java
+++ src/main/java/lotto/LottoNumbersGenerator.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LottoNumbersGenerator {
@@ -11,6 +12,9 @@ public class LottoNumbersGenerator {
     private static final int GENERATE_NUMBER_COUNT = 6;
 
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, GENERATE_NUMBER_COUNT);
+        List<Integer> generatedLottoNumbers = Randoms.pickUniqueNumbersInRange(
+                START_INCLUSIVE, END_INCLUSIVE, GENERATE_NUMBER_COUNT);
+        Collections.sort(generatedLottoNumbers);
+        return generatedLottoNumbers;
     }
 }
