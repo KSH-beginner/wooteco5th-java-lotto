f527d98d720f69088de1254929e9c3c7a0b6c374
ksh-beginner <ohk9134@naver.com>
2022-12-06 19:53:59 +0900

    feat(Lotto) : 발행 번호 간 중복이 있으면 예외 처리 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,10 @@ package lotto.domain;
 import lotto.ErrorConstants;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
@@ -20,10 +23,18 @@ public class Lotto {
         if (isNumbersSizeNotSix(numbers)) {
             throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "로또 번호는 6개여야합니다.");
         }
+        if (isNumbersDuplicate(numbers)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "로또 번호는 중복되면 안됩니다.");
+        }
     }
 
     private boolean isNumbersSizeNotSix(List<Integer> numbers) {
         return numbers.size() != CORRECT_NUMBERS_SIZE;
+    }
+
+    private boolean isNumbersDuplicate(List<Integer> numbers) {
+        Set<Integer> convertedNumbers = new HashSet<>(numbers);
+        return convertedNumbers.size() != CORRECT_NUMBERS_SIZE;
     }
 
     public List<Integer> getNumbers() {
