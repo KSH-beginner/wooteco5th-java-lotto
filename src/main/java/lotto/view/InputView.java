663d2bdc072db0f5fe7e8ec8a1153e9cc82e9f2a
ksh-beginner <ohk9134@naver.com>
2022-12-06 21:42:03 +0900

    feat(InputView) : 쉼표로 구분되지 않는 경우 예외 처리 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,14 +1,17 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.ErrorConstants;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final String LOTTO_WINNING_NUMBERS_SPLIT_DELIMITER = ",";
+    private static final String LOTTO_WINNING_NUMBERS_REGEX = "[0-9]*[,]{0,1}[0-9]+";
 
     public int inputLottoPurchaseMoney() {
         return Integer.parseInt(Console.readLine());
@@ -16,9 +19,24 @@ public class InputView {
 
     public List<Integer> inputLottoWinningNumbers() {
         String lottoWinningNumbers = Console.readLine();
+        validateInputLottoWinningNumbers(lottoWinningNumbers);
+        return splitLottoWinningNumbers(lottoWinningNumbers);
+    }
+
+    private List<Integer> splitLottoWinningNumbers(String lottoWinningNumbers) {
         return Arrays.stream(lottoWinningNumbers.split(LOTTO_WINNING_NUMBERS_SPLIT_DELIMITER))
                 .map(Integer::valueOf)
                 .collect(Collectors.toList());
+    }
+
+    private void validateInputLottoWinningNumbers(String inputLottoWinningNumbers) {
+        if (isNumbersDelimiterNotComma(inputLottoWinningNumbers)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "로또 번호는 쉼표로 구분된 숫자여야합니다.");
+        }
+    }
+
+    private boolean isNumbersDelimiterNotComma(String inputLottoWinningNumbers) {
+        return !Pattern.compile(LOTTO_WINNING_NUMBERS_REGEX).matcher(inputLottoWinningNumbers).matches();
     }
 
     public int inputLottoBonusNumber() {
