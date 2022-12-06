5e411ddeef4e3a238f686ebc907c3f2384671acc
ksh-beginner <ohk9134@naver.com>
2022-12-06 19:49:37 +0900

    feat(Lotto) : 발행 로또 번호 6자리가 아니면 예외 처리 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,14 @@
 package lotto.domain;
 
+import lotto.ErrorConstants;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int CORRECT_NUMBERS_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +17,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (isNumbersSizeNotSix(numbers)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "로또 번호는 6개여야합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isNumbersSizeNotSix(List<Integer> numbers) {
+        return numbers.size() != CORRECT_NUMBERS_SIZE;
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
 }
