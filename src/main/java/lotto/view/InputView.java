918fb266ae4ca3e30ffbe25c06c8c05daf639826
ksh-beginner <ohk9134@naver.com>
2022-12-06 21:49:18 +0900

    feat(InputView) : 로또 번호 1~45 사이의 숫자가 아닌 경우 예외 처리 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,6 +12,8 @@ public class InputView {
 
     private static final String LOTTO_WINNING_NUMBERS_SPLIT_DELIMITER = ",";
     private static final String LOTTO_WINNING_NUMBERS_REGEX = "[0-9]*[,]{0,1}[0-9]+";
+    private static final int MIN_LOTTO_WINNING_NUMBER = 1;
+    private static final int MAX_LOTTO_WINNING_NUMBER = 45;
 
     public int inputLottoPurchaseMoney() {
         return Integer.parseInt(Console.readLine());
@@ -33,10 +35,21 @@ public class InputView {
         if (isNumbersDelimiterNotComma(inputLottoWinningNumbers)) {
             throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "로또 번호는 쉼표로 구분된 숫자여야합니다.");
         }
+        if (isNumbersWrongRange(inputLottoWinningNumbers)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "로또 번호는 1~45 사이의 숫자여야합니다.");
+        }
     }
 
     private boolean isNumbersDelimiterNotComma(String inputLottoWinningNumbers) {
         return !Pattern.compile(LOTTO_WINNING_NUMBERS_REGEX).matcher(inputLottoWinningNumbers).matches();
+    }
+
+    private boolean isNumbersWrongRange(String inputLottoWinningNumbers) {
+        List<Integer> splitNumbers = splitLottoWinningNumbers(inputLottoWinningNumbers);
+        long wrongRangeNumberCount = splitNumbers.stream()
+                .filter(number -> number < MIN_LOTTO_WINNING_NUMBER || number > MAX_LOTTO_WINNING_NUMBER)
+                .count();
+        return wrongRangeNumberCount != 0;
     }
 
     public int inputLottoBonusNumber() {
