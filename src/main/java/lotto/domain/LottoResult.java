dd47ed8ee15f995475fb47beecc4db80a8eaea16
ksh-beginner <ohk9134@naver.com>
2022-12-07 11:27:36 +0900

    feat(LottoResult) : 수익률을 구하는 기능 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -12,23 +12,27 @@ public class LottoResult {
     private static final int THIRD_RANK_COUNT_KEY = 3;
     private static final int FOURTH_RANK_COUNT_KEY = 4;
     private static final int FIFTH_RANK_COUNT_KEY = 5;
+    private static final String YIELD_RATE_FORMAT = "%.1f";
 
     private final Map<Integer, Integer> lottoRankCountMap = new HashMap<>();
     private int winningAmountSum;
+    private double yieldRate;
 
     public LottoResult() {
         initLottoRankCountMap();
     }
 
-    public void generate(List<Lotto> issuedLottos, WinningLotto winningLotto) {
+    public void generate(List<Lotto> issuedLottos, WinningLotto winningLotto, int lottoPurchaseMoney) {
         List<Integer> winningNumbers = winningLotto.getNumbers();
         int bonusNumber = winningLotto.getBonusNumber();
         for (Lotto issueLotto : issuedLottos) {
             LottoRank lottoRank = getLottoRank(winningNumbers, bonusNumber, issueLotto);
             if (lottoRank != LottoRank.NO_RANK) {
                 putLottoRankCountMap(lottoRank);
+                addWinningAmount(lottoRank);
             }
         }
+        calculateYieldRate(winningAmountSum, lottoPurchaseMoney);
     }
 
     private void initLottoRankCountMap() {
@@ -49,15 +53,25 @@ public class LottoResult {
     }
 
     private void putLottoRankCountMap(LottoRank lottoRank) {
-        int rank = lottoRank.getRank(lottoRank);
+        int rank = LottoRank.getRank(lottoRank);
         lottoRankCountMap.put(rank, lottoRankCountMap.get(rank) + 1);
+    }
+
+    private void addWinningAmount(LottoRank lottoRank) {
+        winningAmountSum += LottoRank.getWinningAmount(lottoRank);
+    }
+
+    private void calculateYieldRate(int winningAmountSum, int lottoPurchaseMoney) {
+        String formattingYieldRate = String.format(YIELD_RATE_FORMAT,
+                (double) (winningAmountSum / lottoPurchaseMoney) * 100);
+        yieldRate = Double.parseDouble(formattingYieldRate);
     }
 
     public Map<Integer, Integer> getLottoRankCountMap() {
         return Collections.unmodifiableMap(lottoRankCountMap);
     }
 
-    public int getWinningAmountSum() {
-        return winningAmountSum;
+    public double getYieldRate() {
+        return yieldRate;
     }
 }
